03ff22a76c9e345bdabd1b3cc7d0e853678a17e6
kashcobain <karanshandilya19742001@gmail.com>
2026-07-24 07:43:17 +0530

    day 3

---
+++ week1/day3/tokens.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key=os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key kaha hai bhai")
+
+client=Groq(api_key=my_api_key)
+
+model="llama-3.3-70b-versatile"
+role="user"
+# 3 prompts
+prompt1="hi"
+prompt2="write a essay on ai"
+prompt3="explain timme travel in detail"
+
+# message me role and content
+prompts=[prompt1, prompt2, prompt3]
+for prompt in prompts:
+
+    message={
+        "role": role,
+        "content": prompt
+    }
+
+    messages=[message]
+
+    response=client.chat.completions.create(model=model, messages=messages)
+    usage=response.usage
+    print(f"Prompt: {prompt}--->completion tokens used: {usage.completion_tokens}, prompt tokens used: {usage.prompt_tokens}, total tokens used: {usage.total_tokens} ,m")   
+# print(response)
+
+# print("#######################################")
+
+# answer=response.choices[0].message.content
+# print(answer)
